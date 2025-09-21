49752c0991c319837b89744b15c2a60f3c8da54b
Arushi Jaiswal <24jaiswala_2@rbunagpur.in>
2025-09-21 21:41:17 +0530

    Add the get_stats() method

--- src/tokenizer.py
+++ src/tokenizer.py
@@ -1,9 +1,21 @@
 # src/tokenizer.py
 # Minimal BPE Tokenizer Skeleton
 
+from collections import Counter
+
 class BPETokenizer:
     def __init__(self):
         self.vocab = {}
         self.inverse_vocab = {}
         self.merges = []
         self.vocab_size = 0
+
+    def get_stats(self, corpus):
+        """Count frequency of symbol pairs in corpus."""
+        pairs = Counter()
+        for word in corpus:
+            for i in range(len(word)):   # <-- BUG: should be len(word)-1
+                pairs[(word[i], word[i+1])] += 1
+        return pairs
+
+
