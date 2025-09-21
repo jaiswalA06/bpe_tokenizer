f63c239f5c748cf3ae744a1d29e70b5828b632d0
Arushi Jaiswal <24jaiswala_2@rbunagpur.in>
2025-09-21 21:54:32 +0530

    Fixed the merge_vocab function loop bug by incrementing i

--- src/tokenizer.py
+++ src/tokenizer.py
@@ -14,21 +14,21 @@ class BPETokenizer:
                 pairs[(word[i], word[i+1])] += 1
         return pairs
 
-    def merge_vocab(self, pair, corpus):
-        """Merge the most frequent pair in the corpus."""
-        new_corpus = []
-        replacement = "".join(pair)
-        for word in corpus:
-            new_word, i = [], 0
-            while i < len(word):
-                if i < len(word)-1 and (word[i], word[i+1]) == pair:
-                    new_word.append(replacement)
-                    
-                else:
-                    new_word.append(word[i])
-                    i += 1
-            new_corpus.append(new_word)
-        return new_corpus
+        def merge_vocab(self, pair, corpus):
+            new_corpus = []
+            replacement = "".join(pair)
+            for word in corpus:
+                new_word, i = [], 0
+                while i < len(word):
+                    if i < len(word)-1 and (word[i], word[i+1]) == pair:
+                        new_word.append(replacement)
+                        i += 2   
+                    else:
+                        new_word.append(word[i])
+                        i += 1
+                new_corpus.append(new_word)
+            return new_corpus
+
 
     def train(self, text, vocab_size=50):
         corpus = [list(word) for word in text.strip().split()]
